3c3c0361b2c308692a6ccbac3fecae474f840c7f
practicyj <youngjo9486@gmail.com>
2023-01-18 22:46:10 +0900

    P1000 solved with BufferedReader

--- src/main/java/P1000.java
+++ src/main/java/P1000.java
@@ -1,18 +1,40 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P1000 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String untokendString = bufferedReader.readLine();
-        StringTokenizer stringTokenizer = new StringTokenizer(untokendString," ");
+    BufferedReader bufferedReader;
+    String untokendString;
+    StringTokenizer stringTokenizer;
+    Logger logger = LoggerFactory.getLogger(P1000.class);
+
+    public P1000() throws IOException {
+        logger.debug("A B");
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        untokendString = bufferedReader.readLine();
+        stringTokenizer = new StringTokenizer(untokendString," ");
         int a = Integer.parseInt(stringTokenizer.nextToken());
         int b = Integer.parseInt(stringTokenizer.nextToken());
         if (a > 0) {
             if (b < 10) {
-                System.out.println(a+b);
+                logger.debug("{}",a+b);
+            }
+        }
+        bufferedReader.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        String untokendStr = buffer.readLine();
+        StringTokenizer strTokenizer = new StringTokenizer(untokendStr," ");
+        int a = Integer.parseInt(strTokenizer.nextToken());
+        int b = Integer.parseInt(strTokenizer.nextToken());
+        if (a > 0) {
+            if (b < 10) {
             }
         }
     }
